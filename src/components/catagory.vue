4870f33c9cf8c42bc26a2e60b6222f0dd7bae60e
ketsebaotgizachew <khubofficial00@gmail.com>
2024-10-06 10:14:46 +0300

    added tag changes on press

--- src/components/catagory.vue
+++ src/components/catagory.vue
@@ -1,34 +1,129 @@
 <script setup>
 import { IconChevronRight } from '@tabler/icons-vue';
+import tag from './tag.vue';
+import { ref } from 'vue';
 
-let tags = [
-    "Gadgets",
-    "Clothing",
-    "Footwear",
-    "Accessories",
-    "Home Decor",
-    "Furniture",
-    "Stationary",
-    "Books",
-    "Toys",
-    "Sports",
-    "Fitness",
-    "Health",
-]
+let tags = ref([
+    {
+        name: "Gadgets",
+        active: true
+    },
+    {
+        name: "Clothing",
+        active: false
+    },
+    {
+        name: "Books",
+        active: false
+    },
+    {
+        name: "Furniture",
+        active: false
+    },
+    {
+        name: "Electronics",
+        active: false
+    },
+    {
+        name: "Accessories",
+        active: false
+    },
+    {
+        name: "Toys",
+        active: false
+    },
+    {
+        name: "Stationary",
+        active: false
+    },
+    {
+        name: "Home Appliances",
+        active: false
+    },
+    {
+        name: "Kitchen Appliances",
+        active: false
+    },
+    {
+        name: "Sports",
+        active: false
+    },
+    {
+        name: "Fitness",
+        active: false
+    },
+    {
+        name: "Health",
+        active: false
+    },
+    {
+        name: "Beauty",
+        active: false
+    },
+    {
+        name: "Grocery",
+        active: false
+    },
+    {
+        name: "Automobile",
+        active: false
+    },
+    {
+        name: "Pets",
+        active: false
+    },
+    {
+        name: "Services",
+        active: false
+    },
+    {
+        name: "Education",
+        active: false
+    },
+    {
+        name: "Entertainment",
+        active: false
+    },
+    {
+        name: "Travel",
+        active: false
+    },
+    {
+        name: "Real Estate",
+        active: false
+    },
+    {
+        name: "Jobs",
+        active: false
+    },
+    {
+        name: "Others",
+        active: false
+    }
+])
+
+
 </script>
 
 <template>
-    <div class="flex w-[100vw] h-16 place-items-center px-10 gap-3 overflow-x-scroll">
-        <div class="w-48 min-w-48 rounded-2xl h-14 bg-background flex place-items-center justify-center gap-2 cursor-pointer">
+    <div class="h-scroll-area flex w-[100vw] h-16 place-items-center px-10 gap-3 overflow-x-scroll">
+        <div class="w-48 min-w-48 rounded-2xl h-12 bg-background flex place-items-center justify-center gap-2 cursor-pointer">
             <h1 class="text-textmain font-main font-[800] text-[15px]">All Catagories</h1>
             <IconChevronRight stroke="2" color="black"/>
         </div>
-        <div class="w-32 min-w-32 h-14 bg-background rounded-2xl flex place-items-center justify-center" v-for="(tag,tagindex) in tags" :key="tagindex">
-            <h1 class="text-textmain font-main font-[800] text-[17px]">{{tag}}</h1>
-        </div>
+        <tag :taglist="tags" />
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+    .h-scroll-area{
+        &::-webkit-scrollbar{
+            display: none;
+        }
+    }
+    .tag{
+        &:hover{
+            @apply w-36;
+        }
+    }
 </style>
---
+++ src/components/tag.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { IconX } from '@tabler/icons-vue';
+import { ref } from 'vue';
+
+const props = defineProps({
+    taglist: Object
+});
+
+
+</script>
+
+<template>
+    <div v-for="(tag, tagindex) in props.taglist" :key="tagindex" :class="{'bg-pageBackground border border-white':tag.active}" class="min-w-min px-5 gap-2 h-12 bg-background rounded-2xl flex place-items-center justify-center cursor-pointer tag" @click="tag.active=true">
+        <h1 class="text-textmain font-main font-[800] text-[15px] text-nowrap">{{tag.name}}</h1>
+        <div v-if="tag.active" class=" w-6 h-6 rounded-full flex place-items-center justify-center" @click.stop="tag.active=false">
+            <IconX stroke="2" color="black" class="w-5 h-5"/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
