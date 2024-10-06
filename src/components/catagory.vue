9136be9fb383a047707d16628de4b3075ba40463
ketsebaotgizachew <khubofficial00@gmail.com>
2024-10-06 09:17:22 +0300

    improved search section

--- src/components/catagory.vue
+++ src/components/catagory.vue
@@ -1,17 +1,31 @@
 <script setup>
 import { IconChevronRight } from '@tabler/icons-vue';
+
+let tags = [
+    "Gadgets",
+    "Clothing",
+    "Footwear",
+    "Accessories",
+    "Home Decor",
+    "Furniture",
+    "Stationary",
+    "Books",
+    "Toys",
+    "Sports",
+    "Fitness",
+    "Health",
+]
 </script>
 
 <template>
-    <div class="flex w-screen h-16 place-items-center px-10 gap-3">
-        <div class="w-48 rounded-2xl h-14 bg-background flex place-items-center justify-center gap-2 cursor-pointer">
+    <div class="flex w-[100vw] h-16 place-items-center px-10 gap-3 overflow-x-scroll">
+        <div class="w-48 min-w-48 rounded-2xl h-14 bg-background flex place-items-center justify-center gap-2 cursor-pointer">
             <h1 class="text-textmain font-main font-[800] text-[15px]">All Catagories</h1>
             <IconChevronRight stroke="2" color="black"/>
         </div>
-        <div class="searchicon z-30 w-12 h-12 flex place-items-center justify-center bg-pageBackground rounded-2xl ml-1">
-            <img src="./icons/search.svg" alt="">
+        <div class="w-32 min-w-32 h-14 bg-background rounded-2xl flex place-items-center justify-center" v-for="(tag,tagindex) in tags" :key="tagindex">
+            <h1 class="text-textmain font-main font-[800] text-[17px]">{{tag}}</h1>
         </div>
-        <input type="text" class="w-72 h-14 bg-background rounded-2xl ml-[-65px] pl-16 outline-none placeholder-textmain placeholder:font-bold placeholder:font-main placeholder:text-textmain" placeholder="search for a product">
     </div>
 </template>
 
