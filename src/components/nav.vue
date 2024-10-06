cc475e101e40f36792bbc23ac40b25b2fca381b3
ketsebaotgizachew <khubofficial00@gmail.com>
2024-10-06 10:27:49 +0300

    improved navbar

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,6 +1,15 @@
 <script setup>
 import { gsap } from 'gsap';
 import { onMounted, ref } from 'vue';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 const search = ref(null);
 
@@ -18,20 +27,42 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="w-screen h-[100px] flex place-items-center px-16 gap-4">
+    <div class="w-screen h-[100px] flex place-items-center px-16 gap-3">
         <h1 class="font-main font-[800] text-[20px]">EMARVEL</h1>
         <input ref="search" type="text" class="search w-72 h-12 rounded-2xl px-3 font-main font-bold text-sm ml-auto outline-none" placeholder="Search Products">
-        <img src="./icons/search.svg" alt="" class="ml-[-60px] z-10">
+        <img src="./icons/search.svg" alt="" class="ml-[-55px] z-10">
         <div class="navbtns">
             <img src="./icons/fav.svg" alt="">
         </div>
         <div class="navbtns">
             <img src="./icons/cart.svg" alt="">
         </div>
+        <div class="sampleProfile w-12 h-12 rounded-full cursor-pointer">
+
+        </div>
+        <!--Language Select-->
+        <Select>
+            <SelectTrigger class="w-20">
+                <SelectValue placeholder="En" class="font-main text-textmain font-bold"></SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectItem value="English">En</SelectItem>
+                    <SelectItem value="Amharic">Am</SelectItem>
+                    <SelectItem value="Oromigna">Or</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
     </div>
 </template>
 
 <style lang="scss" scoped>
+    .sampleProfile{
+        background-image: url("./icons/sampleprofile.jpg");
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+    }
     .search{
         transition: 0.2s ease-in-out;
         &::placeholder{
